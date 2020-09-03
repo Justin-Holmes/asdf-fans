456d08580a072d0d7fbaed88e13dc7b75957b112
Justin Holmes <jholmes@cloudflare.com>
2020-09-03 16:37:18 -0500

    Log cookies

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,10 @@
+require 'pp'
+
 class TodosController < ApplicationController
   def index
+    puts "**********COOKIES**************"
+    puts request.headers["HTTP_COOKIE"].split(';')
+    puts "*******************************"
     @todos = Todo.all
   end
 end
